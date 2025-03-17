556920055842f1a0c2dcd6dc1de4412ac00c249c
Shofikul Islam <168334396+xofikul1337@users.noreply.github.com>
2025-03-17 18:49:16 +0600

    Create lite.dart

---
+++ responsive/lite.dart
@@ -0,0 +1,7 @@
+double screenHeight = MediaQuery.of(context).size.height;
+double screenWidth = MediaQuery.of(context).size.width;
+
+Container(
+  height: screenHeight * 0.3,
+  width: screenWidth * 0.8,
+)
